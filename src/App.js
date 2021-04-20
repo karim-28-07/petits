ec3c17da9e13d01a5bba65f31668e26fefb7386e
Mugi <g.mugi93140@gmail.com>
2021-04-20 12:23:17 +0200

    test projet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: 0,
+      numCase: 0
+    }
+
+    this.lancerDee = this.lancerDee.bind(this)
+  }
+
+  lancerDee() {
+    const dee = Math.floor(Math.random() * (6 - 1)) + 1;
+
+    this.setState({
+      numCase: this.state.numCase + dee
+    })
+  }
+
+  // Bonus
+  // go() {
+  //   if (dee === 6) {
+
+
+  //   } else {
+  //     window.alert("noob")
+  //   }
+  // }
+
+  render() {
+
+    console.log("this.state.numCase", this.state.numCase);
+
+    return (
+      <div>
+        <div id="jeu">
+          <div id="col1">
+            <i class="fas fa-circle" style={{color: this.state.numCase == 1 ? "white" : "black"}}></i>
+            <i class="fas fa-circle"></i>
+            <i class="fas fa-circle"></i>
+            <i class="fas fa-circle"></i>
+            <i class="fas fa-circle"></i>
+          </div>
+
+          <div id="col2">
+            <i class="fas fa-circle"></i>
+            <i class="fas fa-circle"></i>
+            <i class="fas fa-circle"></i>
+          </div>
+
+          <div id="col3">
+            <i class="fas fa-circle"></i>
+            <i class="fas fa-circle"></i>
+            <i class="fas fa-circle"></i>
+            <i class="fas fa-circle"></i>
+            <i class="fas fa-circle"></i>
+          </div>
+
+          <div id="col4">
+            <i class="fas fa-circle"></i>
+            <i class="fas fa-circle"></i>
+            <i class="fas fa-circle"></i>
+          </div>
+
+        </div>
+
+        <div>
+            <button onClick={this.lancerDee}>Lancer le Dé</button>
+            <p></p>
+        </div>
+      </div>
+    )
+  }
+
+
 }
 
-export default App;
+export default App
+
