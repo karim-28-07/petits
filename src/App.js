96de5b7bafff5375b516a613b9d7b064231bd29b
Mugi <g.mugi93140@gmail.com>
2021-04-20 15:29:36 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -6,30 +6,40 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      total: 0,
+      total: false,
       numCase: 0
     }
 
     this.lancerDee = this.lancerDee.bind(this)
   }
 
+  
   lancerDee() {
-    const dee = Math.floor(Math.random() * (6 - 1)) + 1;
+    if (this.state.total===true){
+       const dee = Math.floor(Math.random() * (6 - 1)) + 1;
+    console.log(dee)
 
     this.setState({
       numCase: this.state.numCase + dee
     })
+    }
+   
   }
 
   // Bonus
   // go() {
-  //   if (dee === 6) {
+  //  if(dee===6){
+  //    return this.setState({total:true})
 
-
-  //   } else {
+  //  }
+    
+  //   else {
   //     window.alert("noob")
   //   }
   // }
+
+
+
 
   render() {
 
@@ -39,7 +49,7 @@ class App extends React.Component {
       <div>
         <div id="jeu">
           <div id="col1">
-            <i class="fas fa-circle" style={{color: this.state.numCase == 1 ? "white" : "black"}}></i>
+            <i class="fas fa-circle" style={{ color: this.state.numCase == 1 ? "white" : "black" }}></i>
             <i class="fas fa-circle"></i>
             <i class="fas fa-circle"></i>
             <i class="fas fa-circle"></i>
@@ -69,8 +79,8 @@ class App extends React.Component {
         </div>
 
         <div>
-            <button onClick={this.lancerDee}>Lancer le Dé</button>
-            <p></p>
+          <button onClick={this.go}>Lancer le Dé</button>
+          <p></p>
         </div>
       </div>
     )
